12d7b45c12baaa9270a96c184d1d4d4a49310253
Jonathan Cain <jcainuk@gmail.com>
2022-05-17 14:23:07 +0100

    saved the form element to a variable

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,6 @@
 const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
+const formElement = document.querySelector('form');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
