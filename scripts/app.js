d8248064cc21ee630806d69d457e614c674c39e1
Jonathan Cain <jcainuk@gmail.com>
2022-05-18 13:23:01 +0100

    saved the active-player-name span element as a variable

--- scripts/app.js
+++ scripts/app.js
@@ -19,6 +19,7 @@ const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-errors');
 const gameAreaElement = document.getElementById('active-game');
+const activePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
