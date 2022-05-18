35f333986caa4c2d95b54fd0d303d50325afd1bb
Jonathan Cain <jcainuk@gmail.com>
2022-05-18 13:14:14 +0100

    create the switchPlayer function

--- scripts/game.js
+++ scripts/game.js
@@ -6,6 +6,14 @@ const startNewGame = () => {
   gameAreaElement.style.display = 'block';
 };
 
+const switchPlayer = () => {
+  if (activePlayer === 0) {
+    activePlayer = 1;
+  } else {
+    activePlayer = 0;
+  }
+};
+
 const selectGameField = (event) => {
   event.target.textContent = players[activePlayer].symbol;
   event.target.classList.add('disabled');
